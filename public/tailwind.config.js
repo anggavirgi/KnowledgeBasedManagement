cccaef6e8f651841579818dfc316ca2b05436a50
arrafee <arrafi0225@gmail.com>
2023-08-08 13:56:34 +0700

    update tailwind

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['../app/Views/**/*.{php}'],
+  content: ["../app/Views/**/*.php"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
