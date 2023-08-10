9f80bca25aff1b94ecb382c6830541422e6d3e68
Angga <virgi.angga@gmail.com>
2023-08-10 17:59:35 +0700

    update login

--- app/Views/login/register.php
+++ app/Views/login/register.php
@@ -23,7 +23,7 @@
     </div>
 
     <!-- LOGIN CARD -->
-    <div class="absolute w-[38%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white h-auto py-10 px-10 rounded-3xl drop-shadow-lg">
+    <div class="absolute w-[38%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white h-auto py-6 px-10 rounded-3xl drop-shadow-lg">
       <div>Welcome, please register first.</div>
       <div class="text-[#18A8D8] text-[2.1rem] font-bold mt-2 mb-5">Sign up</div>
       <form action="" method="" class="flex flex-col">
@@ -44,14 +44,14 @@
         
         <label for="password">Create password</label>
         <div class="relative flex items-center justify-end mb-1">
-          <input type="text" name="password" id="password" placeholder="Password" class="w-full mt-2 px-4 py-2 rounded-md border border-gray-400 outline-[#18A8D8]">
+          <input type="password" name="password" id="password" placeholder="Password" class="w-full mt-2 px-4 py-2 rounded-md border border-gray-400 outline-[#18A8D8]">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-eye absolute mt-2 mr-4 cursor-pointer text-gray-400 hover:text-[#18A8D8]" viewBox="0 0 16 16">
             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
           </svg>
         </div>
         <div class="relative flex items-center justify-end mb-3">
-          <input type="text" name="confirm_password" id="confirm_password" placeholder="Confirm Password" class="w-full mt-2 px-4 py-2 rounded-md border border-gray-400 outline-[#18A8D8]">
+          <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" class="w-full mt-2 px-4 py-2 rounded-md border border-gray-400 outline-[#18A8D8]">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-eye absolute mt-2 mr-4 cursor-pointer text-gray-400 hover:text-[#18A8D8]" viewBox="0 0 16 16">
             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
