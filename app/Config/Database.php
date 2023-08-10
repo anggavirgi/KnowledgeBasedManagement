3e73d5480295c04ab9b3c87e02403326259b1866
arrafee <arrafi0225@gmail.com>
2023-08-10 09:53:04 +0700

    update config

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -31,7 +31,7 @@ class Database extends Config
         'password' => '',
         'database' => '',
         'DBDriver' => 'MySQLi',
-        'DBPrefix' => '',
+        'DBPrefix' => 'virtusee',
         'pConnect' => false,
         'DBDebug'  => true,
         'charset'  => 'utf8',
