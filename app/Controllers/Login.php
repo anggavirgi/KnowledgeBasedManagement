076c28691cae5af893eed32de4c6a85073505fa5
Adesvian <adesvian2001@gmail.com>
2023-09-18 11:33:34 +0700

    add library googleClient

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -2,15 +2,32 @@
 
 namespace App\Controllers;
 
+use Google_Client;
+
 class Login extends BaseController
 {
-  public function index()
-  {
-    $data = [
-      'title' => 'Login'
-    ];
 
-    return view('login/login', $data);
+  protected $googleClient;
+
+  public function __construct()
+  {
+    $this->googleClient = new Google_Client();
+    $this->googleClient->setClientId('68235445122-s9tfb97u83qbr80v023b9qriurtuquds.apps.googleusercontent.com');
+    $this->googleClient->setClientSecret('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');
+    $this->googleClient->setRedirectUri('http://localhost:8080/kb/proses');
+    $this->googleClient->addScope('email');
+    $this->googleClient->addScope('profile');
+  }
+
+  public function proses()
+  {
+    $token = $this->googleClient->fetchAccessTokenWithAuthCode($this->request->getVar('code'));
+    if (!isset($token['error'])) {
+      $this->googleClient->setAccessToken($token['access_token']);
+      $googleService = new \Google_Service_Oauth2($this->googleClient);
+      $data = $googleService->userinfo->get();
+      dd($data);
+    }
   }
 
   public function register()
