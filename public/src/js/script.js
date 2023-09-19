c1069c899141f47bc1e462c13d7c9991bd238700
angga <68156108+anggavirgi@users.noreply.github.com>
2023-09-19 10:08:17 +0700

    update drag & drop upload and search navbar

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
-
   // LAYOUT CUSTOMER
+  // DROPDOWN GENERAL QUESTION
+  const navbarPage = window.location.pathname;
+  const navbarLayout = document.getElementById("navbar-layout");
+  if (navbarPage !== "/kb") {
+    navbarLayout.classList.add("md:block");
+    navbarLayout.classList;
+  } else {
+    navbarLayout.classList.remove("md:block");
+  }
+
   $(".collapsible").click(function () {
     var icon = $(this).find(".icon");
     // console.log(icon);
@@ -22,6 +31,27 @@ $(document).ready(function () {
     $(id).slideToggle();
   });
 
+  // DROPDOWN SIDEBAR ARTICLE
+
+  $(".collapsiblesidebar").click(function () {
+    var iconside = $(this).find(".icon");
+
+    if (iconside.hasClass("bi-chevron-down")) {
+      iconside.removeClass("bi-chevron-down").addClass("bi-chevron-up");
+      var titlesidebar = $(this).attr("data-title");
+      var titlesidebar = "#" + titlesidebar;
+      $(titlesidebar).addClass("text-main");
+    } else if (iconside.hasClass("bi-chevron-up")) {
+      iconside.removeClass("bi-chevron-up").addClass("bi-chevron-down");
+      var titlesidebar = $(this).attr("data-title");
+      var titlesidebar = "#" + titlesidebar;
+      $(titlesidebar).removeClass("text-main");
+    }
+
+    var targetside = $(this).attr("data-target");
+    $("#" + targetside).slideToggle();
+  });
+
   // LAYOUT ADMIN
   $(".burger-icon").click(function () {
     $(".sidebar").toggleClass("hidden");
@@ -35,16 +65,17 @@ $(document).ready(function () {
   const parts = activePage.split("/");
   const menuPart = "/" + parts[3];
 
-
   // const navLink = document.querySelectorAll('.navbar a').forEach(link => {
   // if(link.href.includes(activePage) && !link.closest('#dropdownAvatarName') && !link.closest('#navbar-search')){
-  const navLink = document.querySelectorAll('.sidebar-child a').forEach(link => {
-    if(link.href.includes(activePage)){
-      link.classList.add('bg-main')
-      link.classList.add('text-white')
-      link.classList.add('rounded-md')
-    }
-  })
+  const navLink = document
+    .querySelectorAll(".sidebar-child a")
+    .forEach((link) => {
+      if (link.href.includes(activePage)) {
+        link.classList.add("bg-main");
+        link.classList.add("text-white");
+        link.classList.add("rounded-md");
+      }
+    });
 
   // CKEDITOR 5 CLASSIC
   ClassicEditor.create(document.querySelector("#editor")).catch((error) => {
@@ -152,5 +183,75 @@ $(document).ready(function () {
       window.location = $(this).data("href");
     }
   });
-  
+
+  // Show file name in drag & drop box
+  // Select the file input element by its id
+  const fileInput = document.getElementById("dropzone-file");
+
+  // Select the <p> element where you want to display the file name
+  const selectedFileName = document.getElementById("selected-file-name");
+
+  const dragdroptext = document.getElementById("dragdroptext");
+  const formatsizetext = document.getElementById("formatsizetext");
+
+  //Border
+  const dropzone = document.getElementById("dropzone");
+
+  // Add an event listener to the file input element
+  fileInput.addEventListener("change", (event) => {
+    // Get the selected file
+    const selectedFile = event.target.files[0];
+
+    // Check if a file is selected
+    if (selectedFile) {
+      // Update the text content of the <p> element with the file name
+      selectedFileName.textContent = `Selected file: ${selectedFile.name}`;
+      selectedFileName.classList.add("-mt-8");
+      dragdroptext.classList.remove("block");
+      dropzone.classList.add("border-main");
+      formatsizetext.classList.remove("block");
+      dragdroptext.classList.add("hidden");
+      formatsizetext.classList.add("hidden");
+    } else {
+      // If no file is selected, clear the text content
+      selectedFileName.textContent = "";
+      selectedFileName.classList.remove("-mt-8");
+      dragdroptext.classList.remove("hidden");
+      dropzone.classList.remove("border-main");
+      formatsizetext.classList.remove("hidden");
+      dragdroptext.classList.add("block");
+      formatsizetext.classList.add("block");
+    }
+  });
+
+  dropzone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropzone.classList.add("border-main"); // Add a class to highlight the drop area when dragging over it.
+  });
+
+  dropzone.addEventListener("dragleave", () => {
+    dropzone.classList.remove("border-main"); // Remove the highlighting class when dragging leaves the drop area.
+  });
+
+  dropzone.addEventListener("drop", (e) => {
+    e.preventDefault();
+    dropzone.classList.remove("border-main"); // Remove the highlighting class when a file is dropped.
+
+    const files = e.dataTransfer.files;
+    handleFileChange(files);
+  });
+
+  function handleFileChange(files) {
+    if (files.length > 0) {
+      const file = files[0];
+      selectedFileName.textContent = `Selected file: ${file.name}`;
+      selectedFileName.classList.add("-mt-8");
+      dragdroptext.classList.remove("block");
+      dropzone.classList.add("border-main");
+      formatsizetext.classList.remove("block");
+      dragdroptext.classList.add("hidden");
+      formatsizetext.classList.add("hidden");
+      fileInput.files = files; // Assign the selected files to the hidden input for form submission.
+    }
+  }
 });
