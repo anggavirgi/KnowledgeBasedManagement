03ce40a22c6daf94bcf5320a2f26a41b2b07f896
angga <68156108+anggavirgi@users.noreply.github.com>
2023-10-17 16:28:34 +0700

    update home

--- app/Views/customer/searchresult.php
+++ app/Views/customer/searchresult.php
@@ -14,19 +14,21 @@
                     <h3 class="font-bold text-xl uppercase">Frequently Ask Questions</h3>
                 </div>
                 <?php foreach ($contents as $content) : ?>
-                    <div class="pb-3 flex flex-col">
-                        <a href="<?= base_url('kb/personalarticle/personalarticledetail?category=' . $content['name_category'] . '&subcategory=' . $content['name_subcategory'] . '&article=' . $content['slug']) ?>" data-article-id="<?= $content['id']; ?>" class="article-link py-3 font-semibold text-xl text-orange-600"><?= $content['title']; ?></a>
-                        <div class="font-medium text-sm border-b-2 pb-3">
-                            <span><?= $content['name_category']; ?></span>
-                            <span> / </span>
-                            <span><?= $content['name_subcategory']; ?></span>
-                            <span> / </span>
-                            <span><?= $content['name_project']; ?></span>
+                    <a href="<?= base_url('kb/personalarticle/personalarticledetail?category=' . $content['name_category'] . '&subcategory=' . $content['name_subcategory'] . '&article=' . $content['slug']) ?>">
+                        <div class="pb-3 flex flex-col px-2 hover:shadow-md hover:shadow-gray-200">
+                            <div data-article-id="<?= $content['id']; ?>" class="article-link pt-3 pb-2 font-semibold text-xl text-orange-600"><?= $content['title']; ?></div>
+                            <div class="font-medium text-sm border-b-2 pb-3">
+                                <span><?= $content['name_category']; ?></span>
+                                <span> / </span>
+                                <span><?= $content['name_subcategory']; ?></span>
+                                <span> / </span>
+                                <span><?= $content['name_project']; ?></span>
+                            </div>
+                            <span class="flex text-ellipsis pt-3 text-sm overflow-hidden text-justify whitespace-nowrap">
+                                <?= $content['content']; ?>
+                            </span>
                         </div>
-                        <span class="flex text-ellipsis pt-3 text-sm overflow-hidden text-justify whitespace-nowrap">
-                            <?= $content['content']; ?>
-                        </span>
-                    </div>
+                    </a>
                 <?php endforeach; ?>
             <?php } ?>
             <?php if($complains != null){ ?>
@@ -37,12 +39,19 @@
                     <h3 class="font-bold text-xl uppercase">Complains</h3>
                 </div>
                 <?php foreach ($complains as $complain) : ?>
-                    <div class="pb-3 flex flex-col">
-                        <a href="<?= base_url('kb/complain/reply?complainId=' . $complain['slug']) ?>" class="py-3 border-b-2 pb-3 font-semibold text-xl text-orange-600"><?= $complain['subject']; ?></a>
-                        <span class="text-ellipsis pt-3 text-sm overflow-hidden text-justify whitespace-nowrap">
-                            <?= $complain['description']; ?>
-                        </span>
-                    </div>
+                    <a href="<?= base_url('kb/complain/reply?complainId=' . $complain['slug']) ?>">
+                        <div class="pb-3 flex flex-col mb-3 hover:shadow-md hover:shadow-gray-200 px-2">
+                            <div class="py-2 pb-2 font-semibold text-xl text-orange-600"><?php echo $complain['subject'] ?></div>
+                            <div class="flex gap-3 text-sm font-semibold">
+                                <span><?= date("F Y", strtotime($complain['created_at'])); ?></span>
+                                <span><?php echo $complain['name_project'] ?></span>
+                            </div>
+                            <hr class="mb-2 mt-3">
+                            <span class="text-ellipsis text-sm overflow-hidden text-justify whitespace-nowrap">
+                                <?php echo $complain['description'] ?>
+                            </span>
+                        </div>
+                    </a>
                 <?php endforeach; ?>
             <?php } ?>
         </div>
